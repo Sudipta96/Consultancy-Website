22886b1a69d7252b6291ffd6936bc5e8e8995317
Sudipta Sarker <sudiptasarker583@gmail.com>
2023-03-08 12:09:05 +0600

    10th commit - migration file and database files deleted

--- resume/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ResumeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'resume'
-
-    def ready(self):
-        import resume.signals
